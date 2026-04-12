4282da139a9f1faf84f19fc21f9e4d75498242f3
Northstar <1155211153@link.cuhk.edu.hk>
2026-04-12 21:46:31 +0800

    Update daily report email delivery method
    Changed delivery method of daily report emails from deliver_later to deliver_now and updated favorite items retrieval.

--- app/workers/daily_report_worker.rb
+++ app/workers/daily_report_worker.rb
@@ -5,9 +5,8 @@ class DailyReportWorker
     @new_items = Item.where("created_at >= ?", 24.hours.ago)
 
     User.find_each do |user|
-      @favorite_items = user.favorite_items
-
-      UserMailer.daily_report(user, @new_items, @favorite_items).deliver_later
+      @favorite_items = user.favorites
+      UserMailer.daily_report(user, @new_items, @favorite_items).deliver_now
     end
   end
 end
